f8580559ce15a6ab1062f66a205669b669fb24f3
maviva97@gmail.com <maviva97@gmail.com>
2023-09-28 14:26:08 +0300

    modificat clasa

---
+++ src/Issues.java
@@ -0,0 +1,7 @@
+public class Issues {
+    public double 	price=300;
+    public double 	tax=1.2;
+    public int quantity=5;
+    double total=0;
+    String message="I want to buy "+ quantity+" shirt!";
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import java.util.Arrays;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        double 	price=300;
-        double 	tax=1.2;
-        int quantity=5;
-        double total=0;
-        String message="I want to buy "+ quantity+" shirt!";
-        System.out.println(message);
-  total= price*quantity*tax;
-        System.out.println("Total cost with tax is: "  + total);
+Issues shop = new Issues();
+        System.out.println(shop.message);
+        shop.total= shop.price*shop.quantity*shop.tax;
+        System.out.println("Total cost with tax is: "  + shop.total);
     }
 }
